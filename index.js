71c4930e784795cb0aaa2d18a65f0a9e0b1835a1
Diaz Ardian Alvianto <ddaiazardian@gmail.com>
2024-07-05 18:43:34 +0700

    update

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const bodyParser = require("body-parser");
 
 const client = new Client({
   authStrategy: new LocalAuth(),
-  puppeteer: {
-    headless: false,
-  },
+  puppeteer: { headless: true, args: [ '--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-accelerated-2d-canvas', '--no-first-run', '--no-zygote', '--single-process', '--disable-gpu', ]}
 });
 const app = express();
 app.use(bodyParser.json());
