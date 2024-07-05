20509a241649f38dbe369a2bf02e194b35a6224f
Diaz Ardian Alvianto <ddaiazardian@gmail.com>
2024-07-05 18:19:31 +0700

    update

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-const { Client } = require("whatsapp-web.js");
+const { Client, LocalAuth } = require("whatsapp-web.js");
 const express = require("express");
-const qrcode = require('qrcode-terminal');
-const bodyParser = require('body-parser');
+const qrcode = require("qrcode-terminal");
+const bodyParser = require("body-parser");
 
-const client = new Client(
-  {
-    webVersionCache: {
-      type: "remote",
-      remotePath:
-        "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2413.51-beta.html",
-    },
-  }
-);
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+    headless: false,
+  },
+});
 const app = express();
 app.use(bodyParser.json());
 bodyParser.urlencoded({ extended: true });
 
 client.on("qr", (qr) => {
-  qrcode.generate(qr, {small: true});
+  qrcode.generate(qr, { small: true });
   console.log("QR RECEIVED", qr);
 });
 
